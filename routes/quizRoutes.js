679d2177fa5eb168e970dd08c76758dcf633645f
Ghala27 <445818375@kku.edu.sa>
2025-04-12 11:10:42 +0300

    Update quizRoutes.js

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -14,7 +14,7 @@ router.post("/quizzes", (req, res) => {
 // عملية قراءة لعرض نتائج المستخدمين السابقة 
  
 router.get("/quizzes/:userId", (req, res) => { 
-    const sql = "SELECT * FROM quiz_scores WHERE user_id = ?"; 
+    const sql = "SELECT * FROM user_scores WHERE user_id = ?"; 
     db.query(sql, [req.params.userId], (err, results) => { 
         if (err) return res.status(500).json({ error: err.message }); 
         res.status(200).json(results); 
